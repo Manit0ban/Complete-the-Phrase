b8821fb3c18d3724398ac61d9960e13205c3a221
Manit0ban <145370706+Manit0ban@users.noreply.github.com>
2023-11-20 09:53:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Event listeners
+document.getElementById("btn1").addEventListener("click", run1);
+document.getElementById("btn2").addEventListener("click", run2);
+document.getElementById("btn3").addEventListener("click", run3);
+document.getElementById("btn4").addEventListener("click", run4);
+document.getElementById("btn5").addEventListener("click", run5);
+document.getElementById("btn6").addEventListener("click", run6);
+
+// Function
+function run1() {
+    alert(`worse.`);
+}
+
+function run2() {
+    alert(`weather.`);
+}
+
+function run3() {
+    alert(`devil.`);
+}
+
+function run4() {
+    alert(`gain.`);
+}
+
+function run5() {
+    alert(`hay.`);
+}
+
+function run6() {
+    alert(`bush.`);
+}
+
+
+
+
+
+
+
